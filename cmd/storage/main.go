cce19f458df88d0c5b02dd2d95fb2b4eef934acd
Stepan <eremeevst@gmail.com>
2024-11-25 11:09:52 +0300

    go modules init commit

---
+++ cmd/storage/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/meesooqa/storage/internal/storage"
+)
+
+func main() {
+	st := storage.NewStorage()
+
+	file, err := st.Upload("test.txt", []byte("hello"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	// fmt.Println("It uploaded", file)
+
+	restoredFile, err := st.GetByID(file.ID)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("It is restored", restoredFile)
+}
